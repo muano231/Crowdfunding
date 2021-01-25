c88d77595976fe6589e75727ffbb64561ffb61b9
leo terras <leo.terras23@gmail.com>
2021-01-26 00:11:59 +0100

    Leo

--- back/don_process.php
+++ back/don_process.php
@@ -9,7 +9,7 @@ $solde = $_SESSION['solde'];
 if ($montant == 0 && isset($projet_id)){
     echo "Il faut entrer un montant pour effectuer un don "."<a href='../front/espace_don.php?projet=".$projet_id."'>Retourner à la page donnation du produit.</a>";
     exit;
-}elseif($solde <= $montant){
+}elseif($solde < $montant){
     echo "Votre solde est insuffisant, il faut le recharger "."<a href='../front/espace_don.php?projet=".$projet_id."'>Retourner à la page donnation du produit.</a>";
     exit;
 }
