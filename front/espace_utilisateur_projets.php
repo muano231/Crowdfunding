da1d27b6854f717ca85e87c5f45e89eaa2da06e2
leo terras <leo.terras23@gmail.com>
2021-02-02 00:33:52 +0100

    Leo

--- front/espace_utilisateur_projets.php
+++ front/espace_utilisateur_projets.php
@@ -11,8 +11,8 @@ $result = $pdo->prepare("SELECT * FROM projet WHERE utilisateur_id = :id ORDER B
 $result->execute(array('id' => $id));
 //$nb_lignes = $stmt2->fetch();
 
-if ($lignes = $result->fetch()){
-while($lignes = $result->fetch()){
+if ($lignes = $result->fetchAll()){
+foreach($lignes as $ligne){
     ?>
     <div class="card mb-3" style="max-width: 1010px;margin-left:150px;margin-top:50px;">
 
@@ -24,35 +24,35 @@ while($lignes = $result->fetch()){
             <div class="card-body">
                 <h5 class="card-title">Modifier mon projet</h5>
                 <form action="../back/espace_utilisateur_update_projet.php" method="post">
-                <input type="hidden" name="projet_id" id="projet_id" value="<?php echo($lignes['id']); ?>"/>
+                <input type="hidden" name="projet_id" id="projet_id" value="<?php echo($ligne['id']); ?>"/>
                     <label class="form-label" for="projet_nom">Nom du projet :</label>
                     <input class="form-control" type="text" name="projet_nom" id="projet_nom"
-                        value="<?php echo($lignes['nom_projet']); ?>" />
+                        value="<?php echo($ligne['nom_projet']); ?>" />
 
                     <label class="form-label" for="projet_description">Description du projet</label>
                     <input class="form-control" type="text" name="projet_description" id="projet_description"
-                        value="<?php echo($lignes['description_projet']); ?>" />
+                        value="<?php echo($ligne['description_projet']); ?>" />
 
                     <label class="form-label" for="">Date butoir</label>
                     <input class="form-control" type="text" name="" id=""
-                        value="<?php echo($lignes['date_butoir']); ?>" disabled />
+                        value="<?php echo($ligne['date_butoir']); ?>" disabled />
 
                     <label class="form-label" for="email">Objectif :</label>
                     <input class="form-control" type="number" name="email" id="email"
-                        value="<?php echo($lignes['objectif']); ?>" disabled />
+                        value="<?php echo($ligne['objectif']); ?>" disabled />
 
                     <br>
                     <input class="btn btn-primary" type="submit" value="Modifier" />
                 </form>
                 <br/>
 
-                <a href="projet_détail.php?nom_projet=<?php echo $lignes['nom_projet']; ?>" title="Envoyer"><input class="btn btn-primary" type="submit" value="Voir son projet" /></a>
+                <a href="projet_détail.php?nom_projet=<?php echo $ligne['nom_projet']; ?>" title="Envoyer"><input class="btn btn-primary" type="submit" value="Voir son projet" /></a>
                 <br>
                 <br>
 
                 <form action="confirmation_suppression.php" method="post">
                     <input class="form-control" type="hidden" name="id" id="id"
-                        value="<?php echo($lignes['id']); ?>" />
+                        value="<?php echo($ligne['id']); ?>" />
                     <input class="btn btn-danger" type="submit" value="Supprimer le projet" />
                 </form>  
             </div>
